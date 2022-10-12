3d0914b0dea36f16df9791669cea1ddaa656e33b
aladinzecic <aladinzecic10@gmail.com>
2022-10-13 00:32:50 +0200

    push

--- vezba.c
+++ vezba.c
@@ -5,12 +5,49 @@ main(){
     // scanf("%f", &broj);
     // printf("ovde je broj sa dve decimale:%.2f", broj);
     // return 0;
-    int a, b, pomocna;
-    printf("unesite prvi broj a=");
-    scanf("%d", &a);
-    printf("unesite drugi broj b=");
-    scanf("%d", &b);
-    pomocna=a;
-    a=b;
-    printf("%d, %d",b ,pomocna);
+
+
+
+    // int a, b, pomocna;
+    // printf("unesite prvi broj a=");
+    // scanf("%d", &a);
+    // printf("unesite drugi broj b=");
+    // scanf("%d", &b);
+    // pomocna=a;
+    // a=b;
+    // printf("%d, %d",b ,pomocna);
+
+
+    // int a ,b ,c;
+    // printf("unesi broj a:");
+    // scanf("%d", &a);
+    // printf("unesi broj b:");
+    // scanf("%d" ,&b);
+    // c= a + b;
+    // printf("resenje:%d\n",c);
+    // return 0;
+
+
+
+    // int a, b;
+    // printf("unesi prvi broj:");
+    // scanf("%d",&a);
+    // printf("unesi drugi broj:");
+    // scanf("%d",&b);
+    // printf("zbir dva broja:%d\n",a+b);
+    // printf("razlika dva broja:%d\n",a-b);
+    // printf("proizvod dva broja:%d\n",a*b);
+    // printf("razlika dva broja:%d\n",a/b);
+    // return 0;
+
+
+    int a, b;
+    printf("unesi prvu stranicu:");
+    scanf("%d",&a);
+    printf("unesi drugu stranicu:");
+    scanf("%d",&b);
+    printf("obim pravougaonika je:%d\n",2*a+2*b);
+    printf("povrsina je:%d\n",a*b);
+    return 0;
+
 }
