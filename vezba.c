0fe3dd941a1858b71f5f0fd1bef2daac82430939
aladinzecic <aladinzecic10@gmail.com>
2022-10-12 23:45:31 +0200

    push

--- vezba.c
+++ vezba.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main(){
-    printf("hello world");
+main(){
+    float broj;
+    printf("unesite realan broj:");
+    scanf("%f", &broj);
+    printf("ovde je broj sa dve decimale:%.2f", broj);
     return 0;
 }
