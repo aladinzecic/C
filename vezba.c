5160f3630aabfa69380402f14be99069af3d4acc
aladinzecic <aladinzecic10@gmail.com>
2022-10-12 23:57:34 +0200

    push

--- vezba.c
+++ vezba.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 main(){
-    float broj;
-    printf("unesite realan broj:");
-    scanf("%f", &broj);
-    printf("ovde je broj sa dve decimale:%.2f", broj);
-    return 0;
+    // float broj;
+    // printf("unesite realan broj:");
+    // scanf("%f", &broj);
+    // printf("ovde je broj sa dve decimale:%.2f", broj);
+    // return 0;
+    int a, b, pomocna;
+    printf("unesite prvi broj a=");
+    scanf("%d", &a);
+    printf("unesite drugi broj b=");
+    scanf("%d", &b);
+    pomocna=a;
+    a=b;
+    printf("%d, %d",b ,pomocna);
 }
