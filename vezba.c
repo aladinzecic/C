ee317168d18acffdbc27cb85a787015be10f01b1
aladinzecic <aladinzecic10@gmail.com>
2022-10-15 23:46:56 +0200

    push

--- vezba.c
+++ vezba.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 main(){
     // float broj;
     // printf("unesite realan broj:");
@@ -41,13 +42,36 @@ main(){
     // return 0;
 
 
-    int a, b;
-    printf("unesi prvu stranicu:");
-    scanf("%d",&a);
-    printf("unesi drugu stranicu:");
-    scanf("%d",&b);
-    printf("obim pravougaonika je:%d\n",2*a+2*b);
-    printf("povrsina je:%d\n",a*b);
+    // int a, b;
+    // printf("unesi prvu stranicu:");
+    // scanf("%d",&a);
+    // printf("unesi drugu stranicu:");
+    // scanf("%d",&b);
+    // printf("obim pravougaonika je:%d\n",2*a+2*b);
+    // printf("povrsina je:%d\n",a*b);
+    // return 0;
+
+    // int r;
+    // printf("unesi poluprecnik kruga:");
+    // scanf("%d",&r);
+    // printf("\nobim je:%.2f",2*r*3.14);
+    // printf("\npovrsina je:%.2f",3.14*r*r);
+    // return 0;
+
+    // int h, r;
+    // printf("unesi visinu valjka:");
+    // scanf("%d",&h);
+    // printf(" unesi poluprecnik valjka:");
+    // scanf("%d",&r);
+    // printf(" povrsina je:%.2f", 2*r*3.14*(r+h));
+    // printf(" zapremina je:%.2f", r*r*3.14*h);
+    // return 0;
+
+    float x,y;
+    printf("unesi x:");
+    scanf("%f",&x);
+    y=sqrt(x+x*x+x*x*x);
+    printf("y je jednako:%.3f", y);
     return 0;
 
 }
