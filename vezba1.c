5df3710ea55897a3c705cea40d291808f170841d
aladinzecic <aladinzecic10@gmail.com>
2022-10-18 00:36:57 +0200

    push

--- vezba1.c
+++ vezba1.c
@@ -213,6 +213,18 @@ main(){
     printf("unesi tri realna broja:");
     scanf("%d%d%d",&a,&b,&c);
     if((a>b) && (b>c))
-    printf("%d%d%d",a,b,c);
+    printf("%d %d %d",c,b,a);
+    else if((a>b) && (b<c) && (a>c))
+    printf("%d %d %d",b,c,a);
+    else if((a>b) && (b<c) && (a<c))
+    printf("%d %d %d",b,a,c);
+    else if((b>a) && (a>c))
+    printf("%d %d %d",c,a,b);
+    else if((c>a) && (c>b) && (a<b))
+    printf("%d %d %d",a,b,c);
+    else if((b>a) && (c>a) && (c<b))
+    printf("%d %d %d",a,c,b);
+    else if((c>a) && (c>b) && (b<a))
+    printf("%d %d %d",b,a,c);
     return 0;
 }
