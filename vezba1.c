1a275a567a0cb6bd7d00e2b5fa772a65cc472f1e
aladinzecic <aladinzecic10@gmail.com>
2022-10-18 00:48:30 +0200

    push

--- vezba1.c
+++ vezba1.c
@@ -209,22 +209,36 @@ main(){
     // else printf("kvadrat vaseg broja nije izmedju %d i %d",a,b);
     // return 0;
 
-    int a,b,c;
-    printf("unesi tri realna broja:");
-    scanf("%d%d%d",&a,&b,&c);
-    if((a>b) && (b>c))
-    printf("%d %d %d",c,b,a);
-    else if((a>b) && (b<c) && (a>c))
-    printf("%d %d %d",b,c,a);
-    else if((a>b) && (b<c) && (a<c))
-    printf("%d %d %d",b,a,c);
-    else if((b>a) && (a>c))
-    printf("%d %d %d",c,a,b);
-    else if((c>a) && (c>b) && (a<b))
-    printf("%d %d %d",a,b,c);
-    else if((b>a) && (c>a) && (c<b))
-    printf("%d %d %d",a,c,b);
-    else if((c>a) && (c>b) && (b<a))
-    printf("%d %d %d",b,a,c);
+    // int a,b,c;
+    // printf("unesi tri realna broja:");
+    // scanf("%d%d%d",&a,&b,&c);
+    // if((a>b) && (b>c))
+    // printf("%d %d %d",c,b,a);
+    // else if((a>b) && (b<c) && (a>c))
+    // printf("%d %d %d",b,c,a);
+    // else if((a>b) && (b<c) && (a<c))
+    // printf("%d %d %d",b,a,c);
+    // else if((b>a) && (a>c))
+    // printf("%d %d %d",c,a,b);
+    // else if((c>a) && (c>b) && (a<b))
+    // printf("%d %d %d",a,b,c);
+    // else if((b>a) && (c>a) && (c<b))
+    // printf("%d %d %d",a,c,b);
+    // else if((c>a) && (c>b) && (b<a))
+    // printf("%d %d %d",b,a,c);
+    // return 0;
+
+
+    int x,y;
+    printf("unesi x:");
+    scanf("%d",&x);
+    if((x>-2) && (x<=2))
+    y=2*x;
+    else if((x>=5) && (x<7))
+    y=3*x-1;
+    else
+    y=1/x;
+    printf("y=%d",y);
     return 0;
+
 }
