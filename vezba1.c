fb0685f462fdfa0046e9dbd1eb5c4af77f4ca2f0
aladinzecic <aladinzecic10@gmail.com>
2022-10-19 23:57:29 +0200

    push

--- vezba1.c
+++ vezba1.c
@@ -299,42 +299,53 @@ main(){
     // return 0;
 
 
-
-    int a,b,c,D,x1,x2;
-    printf("unesi koeficijente:\na="); 
-    scanf("%d",&a);
-    printf("b=");
-    scanf("%d",&b);
-    printf("b=");
-    scanf("%d",&c);
-    if(a!=0){
+    //final boss
+    // int a,b,c,D,x1,x2;
+    // printf("unesi koeficijente:\na="); 
+    // scanf("%d",&a);
+    // printf("b=");
+    // scanf("%d",&b);
+    // printf("b=");
+    // scanf("%d",&c);
+    // if(a!=0){
         
-        D=b*b-4*a*c;
-        if(D>0){
-        x1=(-b+sqrt(D))/(2*a);
-        x2=(-b-sqrt(D))/(2*a);
-        printf("n\Resenja:\nx1=%d, x2=%d",x1,x2);
-        }
-        else if(D==0){
-        x1=(-b/(2*a));
-        printf("\nResenje:\nx1=x2=%d",x1); 
-        }
-        else {
-            x1=-b/(2*a);
-            x2=sqrt(-D)/(2*a);
-            printf("\nKompleksna resenja:\n");
-            printf("x1=%d+i%d, x2=%d-i%di",x1,x2,x1,x2); 
-        }
-    }
-    else{
-        if(b!=0){
-            x1=-c/b;
-            printf("\nResenje:\nx=%d",x1);
-        }
-        else printf("Sistem nema resenja.");
-    }
-    return 0;
+    //     D=b*b-4*a*c;
+    //     if(D>0){
+    //     x1=(-b+sqrt(D))/(2*a);
+    //     x2=(-b-sqrt(D))/(2*a);
+    //     printf("n\Resenja:\nx1=%d, x2=%d",x1,x2);
+    //     }
+    //     else if(D==0){
+    //     x1=(-b/(2*a));
+    //     printf("\nResenje:\nx1=x2=%d",x1); 
+    //     }
+    //     else {
+    //         x1=-b/(2*a);
+    //         x2=sqrt(-D)/(2*a);
+    //         printf("\nKompleksna resenja:\n");
+    //         printf("x1=%d+i%d, x2=%d-i%di",x1,x2,x1,x2); 
+    //     }
+    // }
+    // else{
+    //     if(b!=0){
+    //         x1=-c/b;
+    //         printf("\nResenje:\nx=%d",x1);
+    //     }
+    //     else printf("Sistem nema resenja.");
+    // }
+    // return 0;
 
     
+
+    // int i;
+    // for (i=1;i<6;i++ )
+    // printf("zdravo\n");
+    // return 0;
+
+
+    int i;
+    for(i=1;i<11;i=i+1)
+    printf("%d\n",i);
+    return 0;
 
 }
