7b4b544fa5e8f2510c339410e3442f86fe16bb7a
aladinzecic <aladinzecic10@gmail.com>
2022-10-20 23:52:57 +0200

    push

--- vezba1.c
+++ vezba1.c
@@ -361,8 +361,14 @@ main(){
     // return 0;
 
 
+    // int i;
+    // for(i=10;i>=1;i=i-1)
+    // printf("%d\n",i);
+    // return 0;
+
+
     int i;
-    for(i=10;i>=1;i=i-1)
-    printf("%d\n",i);
+    for(i=1;i<=5;++i)
+    printf("%d\n",i*i);
     return 0;
 }
