bee7a3e3224fa21d4e58883b2faca06e48ba278f
aladinzecic <aladinzecic10@gmail.com>
2022-12-14 23:51:38 +0100

    push

--- vezba1.c
+++ vezba1.c
@@ -2113,6 +2113,45 @@ main(){
 
 
 
+// int i, n, nizA[MAX], nizB[MAX], nizC[MAX];
+//  printf(" Broj elemenata niza: ");
+//  scanf("%d", &n);
+//  printf(" Elementi niza A: ");
+//  for(i=0; i<n; i++)
+//  scanf("%d", &nizA[i]);
+//  printf(" Elementi niza B: ");
+//  for(i=0; i<n; i++)
+//  scanf("%d", &nizB[i]);
+//  printf(" Elementi niza C: ");
+//  for(i=0; i<n; i++)
+//  {
+//  nizC[i]=nizA[i]+nizB[i];
+//  printf("%d ",nizC[i]);
+//  }
+//  return 0;
+
+
+// int nizA[MAX], nizB[MAX], nizC[MAX], i;
+//  printf (" Elementi niza A: ");
+//  for(i=0; i<MAX; i++)
+//  scanf ("%d", &nizA[i]);
+//  printf (" Elementi niza B: ");
+//  for(i=0; i<MAX; i++)
+//  scanf ("%d", &nizB[i]);
+//  printf (" Elementi niza C: ");
+//  for(i=0; i<MAX; i++)
+//  {
+//  nizC[i]=nizA[i]+nizB[MAX-1-i];
+//  printf ("%d ", nizC[i]);
+//  }
+//  getche();
+//  return 0;
+
+
+// 
+
+
+
 
 
 
